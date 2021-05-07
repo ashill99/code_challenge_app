51a73529119b84bc729a861182930614e06c41b9
Brendan Buckingham <brendan.buckingham@gmail.com>
2021-05-07 16:13:55 -0500

    Bugs

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_07_153124) do
+ActiveRecord::Schema.define(version: 2021_05_07_205914) do
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "phone"
     t.string "email"
     t.datetime "created_at", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+users = User.create([
+  { first_name: "James", last_name: "Holden", phone: "555-555-1234", email: "james.holden@roci.com" },
+  { first_name: "Naomi", last_name: "Nagata", phone: "555-555-5678", email: "naomi.nagata@roci.com" },
+  { first_name: "Amos", last_name: "Burton", phone: "555-555-0987", email: "amos.burton@roci.com" },
+  { first_name: "Alex", last_name: "Kamal", phone: "555-555-4321", email: "alex.kamal@roci.com" },
+  { first_name: "Bobby", last_name: nil, phone: "555-555-1928", email: "bobby.draper@roci.com" },
+])
+
